c1ff34c99dd427d4d2780420075b1b1879a3c37e
Keijiro Takahashi <keijiro@gmail.com>
2013-08-09 07:39:53 +0900

    Replaced Osc.cs

--- Assets/Osc.cs
+++ Assets/Osc.cs
@@ -1,158 +1,165 @@
-// Osc.cs - Extremely minimal OSC implementation
-// Copyright (C) 2011 by Artem Baguinski.
-// For details, see https://github.com/artm/Osc.cs
-
 using System;
-using System.Text;
-using System.Collections;
-using System.Net;
 
-public class Osc
+using OscMessageQueue = System.Collections.Generic.Queue<OscMessage>;
+
+public struct OscMessage
 {
-	public static object[] ToArray(byte[] data)
-	{
-		int offset = 0;
-		string path = Osc.ReadString(data,ref offset);
-		string types = Osc.ReadString(data,ref offset);
-		object[] res = new object[types.Length];
-		res[0] = path;
-		int i = 1;
-		foreach(char t in types.Substring(1)) {
-			switch(t) {
-			case 'i':
-				res[i++] = ReadInt32(data, ref offset);
-				break;
-			case 'f':
-				res[i++] = ReadFloat32(data, ref offset);
-				break;
-			case 's':
-				res[i++] = ReadString(data, ref offset);
-				break;
-			default:
-				throw new NotImplementedException("OSC type '" + t + "' not implemented");
-			}
-		}
-		return res;
-	}
+    public string path;
+    public object[] data;
 
-	public static byte[] FromArray(params object[] args)
-	{
-		StringBuilder types = new StringBuilder(args.Length);
-		int size = 0;
-
-		foreach(object o in args) {
-			types.Append(OscTypeOf(o));
-			size += OscSizeOf(o);
-		}
-		types[0] = ',';
-		size += OscSizeOf(types.Length);
-
-		byte[] data = new byte[size];
-		int offset = 0;
-		Put(args[0] as String, data, ref offset);
-		Put(types.ToString(), data, ref offset);
-		for(int i = 1; i<args.Length; i++) {
-			Put(args[i], data, ref offset);
-		}
-		return data;
-	}
-
-	#region details
-	static String ReadString(byte[] data, ref int offset)
-	{
-		int count = 0;
-		for(; (offset+count)<data.Length && data[offset+count]!=0;count++);
-		String result = Encoding.UTF8.GetString(data, offset, count);
-		offset += PaddedLength(count);
-		return result;
-	}
-
-	static int ReadInt32(byte[] data, ref int offset)
-	{
-		int res = IPAddress.NetworkToHostOrder( BitConverter.ToInt32(data, offset) );
-		offset += 4;
-		return res;
-	}
-
-	static float ReadFloat32(byte[] data, ref int offset)
-	{
-		float res;
-		if (!BitConverter.IsLittleEndian)
-			res = BitConverter.ToSingle(data, offset);
-		else {
-			byte[] sub = new byte[4];
-			Array.Copy(data, offset, sub, 0, 4);
-			Array.Reverse(sub);
-			res = BitConverter.ToSingle(sub, 0);
-		}
-		offset += 4;
-		return res;
-	}
-
-	static int PaddedLength(int l)
-	{
-		return l + 4 - l % 4;
-	}
-
-	static int PaddedLength(String s)
-	{
-		return PaddedLength(s.Length);
-	}
-
-	static int OscSizeOf(object o) {
-		switch (o.GetType().Name) {
-		case "Int32":
-		case "Float32":
-			return 4;
-		case "String":
-			return PaddedLength(o as String);
-		default:
-			throw new NotImplementedException(
-			             String.Format("Conversion from {0} to OSC types not implemented", o.GetType().Name));
-		}
-	}
-
-	static char OscTypeOf(object o) {
-		switch (o.GetType().Name) {
-		case "Int32":
-			return 'i';
-		case "Float32":
-			return 'f';
-		case "String":
-			return 's';
-		default:
-			throw new NotImplementedException(
-			             String.Format("Conversion from {0} to OSC types not implemented", o.GetType().Name));
-		}
-	}
-
-	static void Put(object o, byte[] data, ref int offset)
-	{
-		switch (o.GetType().Name) {
-		case "Int32":
-			Array.Copy(BitConverter.GetBytes( IPAddress.HostToNetworkOrder((int)o)), 0, data, offset, 4);
-			offset += 4;
-			break;
-		case "Float32":
-			byte[] tmp = BitConverter.GetBytes((float)o);
-			if (BitConverter.IsLittleEndian)
-				Array.Reverse(tmp);
-			Array.Copy(tmp, 0, data, offset, 4);
-			offset += 4;
-			break;
-		case "String":
-			Put(o as String, data, ref offset);
-			break;
-		default:
-			throw new NotImplementedException(
-			             String.Format("Conversion from {0} to OSC types not implemented", o.GetType().Name));
-		}
-	}
-	static void Put(String s, byte[] data, ref int offset)
-	{
-		Encoding.ASCII.GetBytes(s,0,s.Length,data,offset);
-		offset += PaddedLength(s);
-	}
-	#endregion
+    public override string ToString ()
+    {
+        var temp = path + ":";
+        foreach (var o in data) {
+            temp += o + ":";
+        }
+        return temp;
+    }
 }
 
+public class OscParser
+{
+    #region General private members
+    OscMessageQueue messageBuffer;
+    #endregion
+
+    #region Temporary read buffer
+    Byte[] readBuffer;
+    int readPoint;
+    #endregion
+
+    #region Public members
+    public int MessageCount {
+        get { return messageBuffer.Count; }
+    }
+
+    public OscParser ()
+    {
+        messageBuffer = new OscMessageQueue ();
+    }
+
+    public OscMessage PopMessage ()
+    {
+        return messageBuffer.Dequeue ();
+    }
+
+    public void FeedData (Byte[] data)
+    {
+        readBuffer = data;
+        readPoint = 0;
+
+        ReadMessage ();
+
+        readBuffer = null;
+    }
+    #endregion
+
+    #region Private methods
+    void ReadMessage ()
+    {
+        var path = ReadString ();
+        
+        if (path == "#bundle") {
+            ReadInt64 ();
+
+            while (true) {
+                if (readPoint >= readBuffer.Length) {
+                    return;
+                }
+                var peek = readBuffer[readPoint];
+                if (peek == '/' || peek == '#') {
+                    ReadMessage ();
+                    return;
+                }
+                var bundleEnd = readPoint + ReadInt32();
+                while (readPoint < bundleEnd) {
+                    ReadMessage();
+                }
+            }
+        }
+
+        var temp = new OscMessage ();
+        temp.path = path;
+
+        var types = ReadString ();
+        temp.data = new object[types.Length - 1];
+        
+        for (var i = 0; i < types.Length - 1; i++) {
+            switch (types [i + 1]) {
+            case 'f':
+                temp.data [i] = ReadFloat32 ();
+                break;
+            case 'i':
+                temp.data [i] = ReadInt32 ();
+                break;
+            case 's':
+                temp.data [i] = ReadString ();
+                break;
+            case 'b':
+                temp.data [i] = ReadBlob ();
+                break;
+            }
+        }
+        
+        messageBuffer.Enqueue (temp);
+    }
+
+    float ReadFloat32 ()
+    {
+        Byte[] temp = {
+            readBuffer [readPoint + 3],
+            readBuffer [readPoint + 2],
+            readBuffer [readPoint + 1],
+            readBuffer [readPoint]
+        };
+        readPoint += 4;
+        return BitConverter.ToSingle (temp, 0);
+    }
+
+    int ReadInt32 ()
+    {
+        int temp =
+            (readBuffer [readPoint + 0] << 24) +
+            (readBuffer [readPoint + 1] << 16) +
+            (readBuffer [readPoint + 2] << 8) +
+            (readBuffer [readPoint + 3]);
+        readPoint += 4;
+        return temp;
+    }
+
+    long ReadInt64 ()
+    {
+        long temp =
+            ((long)readBuffer [readPoint + 0] << 56) +
+            ((long)readBuffer [readPoint + 1] << 48) +
+            ((long)readBuffer [readPoint + 2] << 40) +
+            ((long)readBuffer [readPoint + 3] << 32) +
+            ((long)readBuffer [readPoint + 4] << 24) +
+            ((long)readBuffer [readPoint + 5] << 16) +
+            ((long)readBuffer [readPoint + 6] << 8) +
+            ((long)readBuffer [readPoint + 7]);
+        readPoint += 8;
+        return temp;
+    }
+
+    string ReadString ()
+    {
+        var offset = 0;
+        while (readBuffer[readPoint + offset] != 0) {
+            offset++;
+        }
+        var s = System.Text.Encoding.UTF8.GetString (readBuffer, readPoint, offset);
+        readPoint += (offset + 4) & ~3;
+        return s;
+    }
+
+    Byte[] ReadBlob ()
+    {
+        var length = ReadInt32 ();
+        var temp = new Byte[length];
+        Array.Copy (readBuffer, readPoint, temp, 0, length);
+        readPoint += (length + 3) & ~3;
+        return temp;
+    }
+    #endregion
+}
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ An experiment with Unity and OSC (Open Sound Control).
 
 [Short demo](http://vine.co/v/hbwuvZx2Ybd )
 
-This project uses Osc.cs written by Artem Baguinski.
+This project uses Osc.cs written by me.
 For details, see https://github.com/artm/Osc.cs
 
 All other part of this project is in the public domain. 
